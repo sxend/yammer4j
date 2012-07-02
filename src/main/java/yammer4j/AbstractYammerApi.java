b3ebe77004174cf6dadd72bc886d048d2356f271
sxend <arimitsu.k+github@gmail.com>
2012-07-03 00:41:32 +0900

    可視性変更

--- src/main/java/yammer4j/AbstractYammerApi.java
+++ src/main/java/yammer4j/AbstractYammerApi.java
@@ -4,7 +4,7 @@ abstract class AbstractYammerApi {
 
     public static final String BASE_API_URL="https://www.yammer.com/api/v1";
 
-    final YammerHttpClient client;
+    protected final YammerHttpClient client;
 
     AbstractYammerApi(YammerHttpClient client) {
         this.client = client;
