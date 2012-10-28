470e5c61ad37a3ddac8abf6fb71c5889d40b7717
sxend <arimitsu.k+github@gmail.com>
2012-10-29 01:05:23 +0900

    色々作り直し

---
+++ src/main/java/yammer4j/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package yammer4j.exception;
+
+
+public class BadRequestException extends YammerException{
+
+	private static final long serialVersionUID = 7826406140771426683L;
+
+}
---
+++ src/main/java/yammer4j/exception/UnAuthorizedException.java
@@ -0,0 +1,8 @@
+package yammer4j.exception;
+
+
+public class UnAuthorizedException extends YammerException{
+
+	private static final long serialVersionUID = 6744352373423713787L;
+
+}
