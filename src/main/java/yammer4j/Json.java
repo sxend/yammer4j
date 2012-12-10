90755ccd3a6985b4d7c22b7bc7e6037b63e056b7
sxend <arimitsu.k+github@gmail.com>
2012-12-10 10:57:26 +0900

    ﾇｯﾍｯﾎｰ

---
+++ src/main/java/yammer4j/Json.java
@@ -0,0 +1,29 @@
+package yammer4j;
+
+public final class Json implements Success {
+    public Json(String json) {
+        this.json = json == null ? "" : json;
+    }
+
+    private final String json;
+
+    public String getjson() {
+        return this.json;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 + (json == null ? 0 : json.hashCode());
+    }
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Json){
+            return json.equals(o.toString());
+        }
+        return false;
+    }
+    @Override
+    public String toString() {
+        return this.json;
+    }
+}
